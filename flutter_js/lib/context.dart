da3d7e839b6cd3c62d4e8ea0e41acb2bb1f4470c
dolphin <adaikiss@163.com>
2021-06-06 18:13:05 +0800

    refactor Context into api wrapper on top of vm

--- flutter_js/lib/context.dart
+++ flutter_js/lib/context.dart
@@ -1,130 +1,96 @@
-import 'dart:ffi';
-
 import 'package:flutter/foundation.dart';
 
-import 'lifetime.dart';
 import 'types.dart';
-
-typedef PromiseCallback = void Function(bool success, dynamic value);
-
-int _jsToNativeCallbackIdIncrement = 1;
-
-Map<dynamic, Map<dynamic, Function>> _jsToNativeCallbacks = {};
-
-void __addNativeCallback(String instanceId, dynamic callbackId, Function fn) {
-  Map<dynamic, Function> store =
-      _jsToNativeCallbacks.putIfAbsent(instanceId, () => ({}));
-  store[callbackId] = fn;
-}
-
-int _addNativeCallback(dynamic instanceId, Function fn) {
-  int id = _jsToNativeCallbackIdIncrement++;
-  __addNativeCallback(instanceId, id, fn);
-  return id;
-}
-
-Function? _getNativeCallback(dynamic instanceId, int callbackId,
-    [bool remove = true]) {
-  Map<dynamic, Function>? store = _jsToNativeCallbacks[instanceId];
-  if (store == null) {
-    return null;
-  }
-  Function? fn = store[callbackId];
-  if (fn != null && remove) {
-    store.remove(callbackId);
-  }
-  return fn;
-}
-
-/// Used for test.
-void clearAllNativeCallbacks() {
-  _jsToNativeCallbacks.clear();
-}
+import 'vm.dart';
 
 abstract class JavaScriptContext {
-  static int _instanceIdIncrement = 1;
-  final String instanceId = '${_instanceIdIncrement++}';
 
   late JSValuePointer channelInstance;
 
   @protected
   void postCreate() {
-    channelInstance = jsEval(
-      'const FlutterJS={instanceId:"$instanceId",typeOf:function(obj){return typeof obj}};FlutterJS',
-      name: 'FlutterJS setup',
-    ).value;
-    setupChannelFunction();
   }
-
-  Lifetime<JSValuePointer> jsEval(String js, {String? name});
-
-  Lifetime<JSValuePointer> jsCallFunction(JSValuePointer fn,
-      {List<JSValuePointer>? args, JSValuePointer? thisObject});
-
-  /// convert js value to dart value.
-  ///
-  /// ```
-  /// null->null
-  /// undefined->null
-  /// Number->double
-  /// String->String
-  /// ArrayBuffer->TODO
-  /// Array->List
-  /// Promise->Future
-  /// Function->Function
-  /// Object->Map<String,dynamic>
-  /// ```
-  dynamic jsToDart(JSValuePointer jsValueRef);
-
-  Pointer dartToJs(dynamic val);
-
-  /// Add a callback for internal usage.
-  ///
-  /// Current usage:
-  ///
-  /// 1. Convert dart function to JS function.
-  ///
-  /// Since `Pointer.fromFunction` in ffi doesn't support dynamic functions,
-  /// we reuse the sendMessage mechanism to provide a proxy function for JS.
-  /// When that function is invoked, it simply call `FlutterJS.sendMessage` to
-  /// transfer the calling, and returns result from native.
-  ///
-  /// 2. Convert JS promise to dart Future.
-  ///
-  /// It's similar to the previous one. While converting JS promise to dart Future,
-  /// the promise's then function is invoked with an onFulfilled callback and an onError callback,
-  /// each one send result to dart through `FlutterJS.sendMessage`.
-  ///
-  /// Note: parameters defined in [fn] should follow the result types of [jsToDart], especially for numbers.
-  /// Use double to receive numbers and convert them to int in the function body.
-  @protected
-  int addNativeCallback(Function fn) {
-    return _addNativeCallback(instanceId, fn);
-  }
-
-  /// Add a bridge callback. It is invoked when calling following JS code
-  ///
-  /// ```javascript
-  /// FlutterJS.sendMessage("$channelName", args);
-  /// ```
-  ///
-  /// [fn] receives a single parameter of type [List] and might return any data that can be converted to JS value through [dartToJs].
-  @protected
-  void addBridgeCallback(String channelName, Function fn) {
-    __addNativeCallback(instanceId, channelName, fn);
-  }
-
-  /// Int [callbackId] for internal functions, String for setupBridge functions.
-  @protected
-  Function? getNativeCallback(dynamic callbackId, [bool remove = true]) {
-    return _getNativeCallback(instanceId, callbackId, remove);
-  }
-
-  @protected
-  void setupChannelFunction();
 
   @mustCallSuper
   void dispose() {
-    _jsToNativeCallbacks.remove(instanceId);
   }
+
+  /// Evaluate a JS [code] and return the result.
+  JSValuePointer eval(String code, {String? name});
+
+  /// Call a JS function [fn] with [args] and [thisObject].
+  ///
+  /// **Note:** The caller has respond to free all the arguments, ie: [fn], [args], [thisObject].
+  JSValuePointer callFunction(JSValuePointer fn,
+      {List<JSValuePointer>? args, JSValuePointer? thisObject});
+
+  /// Convert a JS value [jsValueRef] into dart value.
+  dynamic jsToDart(JSValuePointer jsValueRef);
+
+  /// Convert a dart value [val] into JS value.
+  JSValuePointer dartToJS(dynamic val);
+
+  /// Call this method when you are using asynchronous code inside the QuickJSVm.
+  /// The number of executed pending jobs is returned.
+  ///
+  /// There is no event loop in QuickJS.
+  /// You need to call this method to execute pending job of promises.
+  ///
+  /// **Note:** It is safe to call this method multiple times and calling this method on iOS(which uses JavaScriptCore) has no effect.
+  ///
+  int executePendingJob() {
+    return 0;
+  }
+
+  /// Register a module with name [moduleName] to this context, and can be `require`d later.
+  ///
+  /// Since the Vm is rarely reused(prefer to create a new vm for each eval call), the result of ModuleResolver is not cached, which is different from NodeJS.
+  ///
+  /// If you do need to cache the result of `require` call, have a look at `module_loader_test.dart`.
+  void registerModule(String moduleName, ModuleResolver resolver);
+
+  // /// Add a callback for internal usage.
+  // ///
+  // /// Current usage:
+  // ///
+  // /// 1. Convert dart function to JS function.
+  // ///
+  // /// Since `Pointer.fromFunction` in ffi doesn't support dynamic functions,
+  // /// we reuse the sendMessage mechanism to provide a proxy function for JS.
+  // /// When that function is invoked, it simply call `FlutterJS.sendMessage` to
+  // /// transfer the calling, and returns result from native.
+  // ///
+  // /// 2. Convert JS promise to dart Future.
+  // ///
+  // /// It's similar to the previous one. While converting JS promise to dart Future,
+  // /// the promise's then function is invoked with an onFulfilled callback and an onError callback,
+  // /// each one send result to dart through `FlutterJS.sendMessage`.
+  // ///
+  // /// Note: parameters defined in [fn] should follow the result types of [jsToDart], especially for numbers.
+  // /// Use double to receive numbers and convert them to int in the function body.
+  // @protected
+  // int addNativeCallback(Function fn) {
+  //   return _addNativeCallback(instanceId, fn);
+  // }
+
+  // /// Add a bridge callback. It is invoked when calling following JS code
+  // ///
+  // /// ```javascript
+  // /// FlutterJS.sendMessage("$channelName", args);
+  // /// ```
+  // ///
+  // /// [fn] receives a single parameter of type [List] and might return any data that can be converted to JS value through [dartToJS].
+  // @protected
+  // void addBridgeCallback(String channelName, Function fn) {
+  //   __addNativeCallback(instanceId, channelName, fn);
+  // }
+
+  // /// Int [callbackId] for internal functions, String for setupBridge functions.
+  // @protected
+  // Function? getNativeCallback(dynamic callbackId, [bool remove = true]) {
+  //   return _getNativeCallback(instanceId, callbackId, remove);
+  // }
+
+  // @protected
+  // void setupChannelFunction();
 }
