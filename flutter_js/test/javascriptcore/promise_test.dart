11b6eebd053a304c4467eebe7f98ecb64fd067b9
dolphin <adaikiss@163.com>
2021-06-07 08:16:18 +0800

    add unit tests aka examples

---
+++ flutter_js/test/javascriptcore/promise_test.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter_js/javascriptcore/vm.dart';
+import 'package:flutter_js/types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('promise', () {
+    late JavaScriptCoreVm vm;
+    setUp(() {
+      vm = JavaScriptCoreVm();
+    });
+    tearDown(() {
+      vm.dispose();
+    });
+    test('nested promise', () async {
+      JSToDartFunction fn = (args, {thisObj}) {
+        return vm.dartToJS(Future.delayed(Duration(seconds: 2), () => Future.delayed(Duration(seconds: 2), () => ({'message': 'Hello World!', 'year': 2021}))));
+      };
+
+      vm.setProperty(vm.global, 'fn', vm.newFunction(null, fn));
+      vm.startEventLoop();
+      final actual = await vm.jsToDart(vm.evalCode(r'fn()'));
+      expect(actual, {'message': 'Hello World!', 'year': 2021});
+    });
+  });
+}
