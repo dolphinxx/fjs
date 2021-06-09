bafac7f9aacbc18ca9315d10d77192b4d9c14e1c
dolphin <adaikiss@163.com>
2021-06-10 07:41:58 +0800

    remove unused code

--- flutter_js/lib/javascriptcore/jscore/js_property_name_accumulator.dart
+++
@@ -1,9 +0,0 @@
-// import 'dart:ffi';
-//
-// /// An ordered set used to collect the names of a JavaScript object's properties.
-// class JSPropertyNameAccumulator {
-//   /// C pointer
-//   final Pointer pointer;
-//
-//   JSPropertyNameAccumulator(this.pointer);
-// }
