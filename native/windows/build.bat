90daa843151be6b0eec135a7f258a1ed181e3beb
dolphin <adaikiss@163.com>
2021-06-11 16:20:04 +0800

    merge prebuild.bat into build.bat

--- native/windows/build.bat
+++ native/windows/build.bat
@@ -6,7 +6,12 @@ echo "Setup MSVC env..."
 call "D:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 echo "Copy source files to build..."
-call prebuild.bat
+
+if exist .\build\ rmdir .\build\ /s /q
+if not exist .\build\ mkdir build
+for %%f in (Makefile,VERSION,cutils.c,cutils.h,libbf.c,libbf.h,libregexp-opcode.h,libregexp.c,libregexp.h,libunicode-table.h,libunicode.c,libunicode.h,list.h,qjs.c,qjsc.c,quickjs-atom.h,quickjs-libc.c,quickjs-libc.h,quickjs-opcode.h,quickjs.c,quickjs.h,unicode_gen.c,unicode_gen_def.h) do xcopy /s /i ..\src\quickjs\%%f .\build\ /Y
+for %%f in (interface.cpp,quickjs.def) do xcopy /s /i ..\src\%%f .\build\ /Y
+for %%f in (cutils.h,quickjs.c,quickjs.h) do xcopy /s /i ..\src\patch\%%f .\build\ /Y
 
 echo "Building..."
 cmake -S . -B .\build -DCMAKE_BUILD_TYPE=Release -G "Ninja"
